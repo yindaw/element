62966f583c0de28bbd14eb1d3cdbfd71ca5644da
jieGit <2857705371@qq.com>
2021-01-07 13:08:15 +0800

    3.b

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,63 @@
 <template>
   <div id="app">
       <el-container direction="vertical">
-          <el-header>Header</el-header>
+          <el-header height="200px">Header</el-header>
           <el-main>Main</el-main>
       </el-container>
+      <br>
+      <hr>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+      <br>
+      <hr>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container> 
+
+      <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+      </el-container>
+
+
+      <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+            <el-main>Main</el-main>
+            <el-footer>Footer</el-footer>
+            </el-container>
+        </el-container>
+        </el-container>
+        </el-container>
+
+
+
+        <el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+
+
+
+<el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</el-container>
   </div>
 </template>
 
--- src/component/element/footer/src/Footer.vue
+++ src/component/element/footer/src/Footer.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="el-footer">
+    <div class="el-footer" :style="{ height }">
         <slot></slot>
     </div>
 </template>
 <script>
 export default {
-    name: "ElFooter"
+    name: "ElFooter",
+    props: {
+        height: {
+            type: String,
+            default: "60px"
+        }
+    }
 }
 </script>
