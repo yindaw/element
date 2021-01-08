3d99d012cc44579fae914bb4482f828fb14017ce
jieGit <2857705371@qq.com>
2021-01-08 14:56:14 +0800

    7.c

--- src/component/element/button/src/Button.vue
+++ src/component/element/button/src/Button.vue
@@ -1,4 +1,19 @@
 <template>
-    <button></button>
+    <button
+    class="el-button"
+    :class="[
+    type ? `el-button--${type}` : '',
+    plain ? 'is-plain' : '']">
+         <slot></slot>
+    </button>
 </template>
+<script>
+export default {
+    name: "ElButton",
+    props: {
+        type: String, 
+        plain: Boolean,
 
+    }
+}
+</script>
